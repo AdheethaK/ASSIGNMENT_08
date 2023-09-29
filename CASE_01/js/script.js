efba1d49ebbafc5354518ce85291cd75f5fb3fb6
ANK <kalupahanaadheetha5@gmail.com>
2023-09-30 03:10:55 +0530

    update animation into a que in case 01

--- CASE_01/js/script.js
+++ CASE_01/js/script.js
@@ -1,57 +1,312 @@
+
 const myFunction_1 = () =>{
-    $(".img-1").css("opacity",1);
-    $(".img-2").css("opacity",0);
-    $(".img-3").css("opacity",0);
-    $(".img-4").css("opacity",0);
-    $(".img-5").css("opacity",0);
-    $(".img-6").css("opacity",0);
+    //01
+    const image_1 = document.getElementById('img_1');
+    image_1.src = 'img/img-1.jpg';
+    $("#container_1").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '100px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //02
+    const image_2 = document.getElementById('img_2');
+    image_2.src = 'img/img-2.jpg';
+    $("#container_2").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '108px' , bottom: '0', left: '-35', right: '0',
+        margin: 'auto'
+    });
+    //03
+    const image_3 = document.getElementById('img_3');
+    image_3.src = 'img/img-3.jpg';
+    $("#container_3").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '90px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //04
+    const image_4 = document.getElementById('img_4');
+    image_4.src = 'img/img-4.jpg';
+    $("#container_4").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //05
+    const image_5 = document.getElementById('img_5');
+    image_5.src = 'img/img-5.jpg';
+    $("#container_5").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //06
+    const image_6 = document.getElementById('img_6');
+    image_6.src = 'img/img-6.jpg';
+    $("#container_6").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
 }
 const myFunction_2 = () =>{
-    $(".img-1").css("opacity",0);
-    $(".img-2").css("opacity",1);
-    $(".img-3").css("opacity",0);
-    $(".img-4").css("opacity",0);
-    $(".img-5").css("opacity",0);
-    $(".img-6").css("opacity",0);
+    //01
+    const image_1 = document.getElementById('img_1');
+    image_1.src = 'img/img-6.jpg';
+    $("#container_1").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //02
+    const image_2 = document.getElementById('img_2');
+    image_2.src = 'img/img-1.jpg';
+    $("#container_2").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '100px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //03
+    const image_3 = document.getElementById('img_3');
+    image_3.src = 'img/img-2.jpg';
+    $("#container_3").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '108px' , bottom: '0', left: '-35', right: '0',
+        margin: 'auto'
+    });
+    //04
+    const image_4 = document.getElementById('img_4');
+    image_4.src = 'img/img-3.jpg';
+    $("#container_4").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '90px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //05
+    const image_5 = document.getElementById('img_5');
+    image_5.src = 'img/img-4.jpg';
+    $("#container_5").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //06
+    const image_6 = document.getElementById('img_6');
+    image_6.src = 'img/img-5.jpg';
+    $("#container_6").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
 }
 const myFunction_3 = () =>{
-    $(".img-1").css("opacity",0);
-    $(".img-2").css("opacity",0);
-    $(".img-3").css("opacity",1);
-    $(".img-4").css("opacity",0);
-    $(".img-5").css("opacity",0);
-    $(".img-6").css("opacity",0);
+    //01
+    const image_1 = document.getElementById('img_1');
+    image_1.src = 'img/img-5.jpg';
+    $("#container_1").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //02
+    const image_2 = document.getElementById('img_2');
+    image_2.src = 'img/img-6.jpg';
+    $("#container_2").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //03
+    const image_3 = document.getElementById('img_3');
+    image_3.src = 'img/img-1.jpg';
+    $("#container_3").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '100px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //04
+    const image_4 = document.getElementById('img_4');
+    image_4.src = 'img/img-2.jpg';
+    $("#container_4").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '108px' , bottom: '0', left: '-35', right: '0',
+        margin: 'auto'
+    });
+    //05
+    const image_5 = document.getElementById('img_5');
+    image_5.src = 'img/img-3.jpg';
+    $("#container_5").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '90px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //06
+    const image_6 = document.getElementById('img_6');
+    image_6.src = 'img/img-4.jpg';
+    $("#container_6").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
 }
 const myFunction_4 = () =>{
-    $(".img-1").css("opacity",0);
-    $(".img-2").css("opacity",0);
-    $(".img-3").css("opacity",0);
-    $(".img-4").css("opacity",1);
-    $(".img-5").css("opacity",0);
-    $(".img-6").css("opacity",0);
+    //01
+    const image_1 = document.getElementById('img_1');
+    image_1.src = 'img/img-4.jpg';
+    $("#container_1").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //02
+    const image_2 = document.getElementById('img_2');
+    image_2.src = 'img/img-5.jpg';
+    $("#container_2").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //03
+    const image_3 = document.getElementById('img_3');
+    image_3.src = 'img/img-6.jpg';
+    $("#container_3").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //04
+    const image_4 = document.getElementById('img_4');
+    image_4.src = 'img/img-1.jpg';
+    $("#container_4").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '100px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //05
+    const image_5 = document.getElementById('img_5');
+    image_5.src = 'img/img-2.jpg';
+    $("#container_5").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '108px' , bottom: '0', left: '-35', right: '0',
+        margin: 'auto'
+    });
+    //06
+    const image_6 = document.getElementById('img_6');
+    image_6.src = 'img/img-3.jpg';
+    $("#container_6").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '90px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
 }
 const myFunction_5 = () =>{
-    $(".img-1").css("opacity",0);
-    $(".img-2").css("opacity",0);
-    $(".img-3").css("opacity",0);
-    $(".img-4").css("opacity",0);
-    $(".img-5").css("opacity",1);
-    $(".img-6").css("opacity",0);
+    //01
+    const image_1 = document.getElementById('img_1');
+    image_1.src = 'img/img-3.jpg';
+    $("#container_1").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '90px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //02
+    const image_2 = document.getElementById('img_2');
+    image_2.src = 'img/img-4.jpg';
+    $("#container_2").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //03
+    const image_3 = document.getElementById('img_3');
+    image_3.src = 'img/img-5.jpg';
+    $("#container_3").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //04
+    const image_4 = document.getElementById('img_4');
+    image_4.src = 'img/img-6.jpg';
+    $("#container_4").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //05
+    const image_5 = document.getElementById('img_5');
+    image_5.src = 'img/img-1.jpg';
+    $("#container_5").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '100px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //06
+    const image_6 = document.getElementById('img_6');
+    image_6.src = 'img/img-2.jpg';
+    $("#container_6").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '108px' , bottom: '0', left: '-35', right: '0',
+        margin: 'auto'
+    });
 }
 const myFunction_6 = () =>{
-    $(".img-1").css("opacity",0);
-    $(".img-2").css("opacity",0);
-    $(".img-3").css("opacity",0);
-    $(".img-4").css("opacity",0);
-    $(".img-5").css("opacity",0);
-    $(".img-6").css("opacity",1);
+    //01
+    const image_1 = document.getElementById('img_1');
+    image_1.src = 'img/img-2.jpg';
+    $("#container_1").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '108px' , bottom: '0', left: '-35', right: '0',
+        margin: 'auto'
+    });
+    //02
+    const image_2 = document.getElementById('img_2');
+    image_2.src = 'img/img-3.jpg';
+    $("#container_2").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '90px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
+    //03
+    const image_3 = document.getElementById('img_3');
+    image_3.src = 'img/img-4.jpg';
+    $("#container_3").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //04
+    const image_4 = document.getElementById('img_4');
+    image_4.src = 'img/img-5.jpg';
+    $("#container_4").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //05
+    const image_5 = document.getElementById('img_5');
+    image_5.src = 'img/img-6.jpg';
+    $("#container_5").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '120px' , bottom: '0', left: '-38', right: '0',
+        margin: 'auto'
+    });
+    //06
+    const image_6 = document.getElementById('img_6');
+    image_6.src = 'img/img-1.jpg';
+    $("#container_6").css({
+        width:'200px' , height:'auto' ,
+        position: 'absolute' , top: '100px' , bottom: '0', left: '0', right: '0',
+        margin: 'auto'
+    });
 }
-var t = 300;
-for (var i = 0; i < 100; i++) {
-    setTimeout(myFunction_1,t);
-    setTimeout(myFunction_2,t+=300);
-    setTimeout(myFunction_3,t+=300);
-    setTimeout(myFunction_4,t+=300);
-    setTimeout(myFunction_5,t+=300);
-    setTimeout(myFunction_6,t+=300);
-}
+$("#btn-start").on('click',()=>{
+    var t = 800;
+    for (var i = 0; i < 100; i++) {
+        setTimeout(myFunction_1,t);
+        setTimeout(myFunction_2,t+=800);
+        setTimeout(myFunction_3,t+=800);
+        setTimeout(myFunction_4,t+=800);
+        setTimeout(myFunction_5,t+=800);
+        setTimeout(myFunction_6,t+=800);
+    }
+});
