415b5c7a841aa82da69ead1895aa583fe402ae26
ANK <kalupahanaadheetha5@gmail.com>
2023-11-01 02:02:20 +0530

    create functions to transition colors in each unit in script file

---
+++ CASE 02/js/script.js
@@ -0,0 +1,65 @@
+const change_color_1 = () =>{
+    $("#unit_1").addClass("change_color_1");
+    $("#unit_2").addClass("change_color_2");
+    $("#unit_3").addClass("change_color_3");
+    $("#unit_4").addClass("change_color_4");
+    $("#unit_5").addClass("change_color_5");
+    $("#unit_6").addClass("change_color_6");
+    $("#unit_7").addClass("change_color_7");
+}
+const change_color_2 = () =>{
+    $("#unit_1").addClass("change_color_7");
+    $("#unit_2").addClass("change_color_1");
+    $("#unit_3").addClass("change_color_2");
+    $("#unit_4").addClass("change_color_3");
+    $("#unit_5").addClass("change_color_4");
+    $("#unit_6").addClass("change_color_5");
+    $("#unit_7").addClass("change_color_6");
+}
+const change_color_3 = () =>{
+    $("#unit_1").addClass("change_color_6");
+    $("#unit_2").addClass("change_color_7");
+    $("#unit_3").addClass("change_color_1");
+    $("#unit_4").addClass("change_color_2");
+    $("#unit_5").addClass("change_color_3");
+    $("#unit_6").addClass("change_color_4");
+    $("#unit_7").addClass("change_color_5");
+}
+const change_color_4 = () =>{
+    $("#unit_1").addClass("change_color_5");
+    $("#unit_2").addClass("change_color_6");
+    $("#unit_3").addClass("change_color_7");
+    $("#unit_4").addClass("change_color_1");
+    $("#unit_5").addClass("change_color_2");
+    $("#unit_6").addClass("change_color_3");
+    $("#unit_7").addClass("change_color_4");
+}
+const change_color_5 = () =>{
+    $("#unit_1").addClass("change_color_4");
+    $("#unit_2").addClass("change_color_5");
+    $("#unit_3").addClass("change_color_6");
+    $("#unit_4").addClass("change_color_7");
+    $("#unit_5").addClass("change_color_1");
+    $("#unit_6").addClass("change_color_2");
+    $("#unit_7").addClass("change_color_3");
+}
+const change_color_6 = () =>{
+    $("#unit_1").addClass("change_color_3");
+    $("#unit_2").addClass("change_color_4");
+    $("#unit_3").addClass("change_color_5");
+    $("#unit_4").addClass("change_color_6");
+    $("#unit_5").addClass("change_color_7");
+    $("#unit_6").addClass("change_color_1");
+    $("#unit_7").addClass("change_color_2");
+}
+const change_color_7 = () =>{
+    $("#unit_1").addClass("change_color_2");
+    $("#unit_2").addClass("change_color_3");
+    $("#unit_3").addClass("change_color_4");
+    $("#unit_4").addClass("change_color_5");
+    $("#unit_5").addClass("change_color_6");
+    $("#unit_6").addClass("change_color_7");
+    $("#unit_7").addClass("change_color_1");
+}
+
+change_color_1();
